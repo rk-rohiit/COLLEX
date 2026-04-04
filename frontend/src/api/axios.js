e7cdbb92275648ef53df18afde862471c50e65a6
rk-rohiit <askrohiitsharma@gmail.com>
2026-04-04 23:29:19 +0530

    api fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // 🔥 Create Axios Instance
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://collex-soeo.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
